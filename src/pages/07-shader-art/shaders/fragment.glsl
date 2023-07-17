197122ddcd7eec36c41272fd8e85f2041973b541
Benjamin Petermann <benjamin.petermann@gmx.at>
2023-07-17 22:34:29 +0200

    Add first shader to 07

--- src/pages/07-shader-art/shaders/fragment.glsl
+++ src/pages/07-shader-art/shaders/fragment.glsl
@@ -1,15 +1,28 @@
 varying vec2 vUv;
 uniform float uTime;
 
+vec3 palette( float t ) {
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(2.0, 1.0, 0.0);
+    vec3 d = vec3(0.5,0.2,0.25);
+
+    return a + b*cos( 6.28318*(c*t+d) );
+}
+
 void main()
 {
-    vec3 final = vec3(vUv.x, vUv.x, 1.0);
+    vec3 finalColor = vec3(0.0);
+    vec2 uv = vec2(vUv.x * vUv.y);
 
-    // float strength = mod(vUv.x * vUv.y + sin(uTime * 0.2) * 8.0, 1.0);
-    // strength = abs(strength);
-    // strength = step(0.5, strength);
+   for (float i = 0.0; i < 4.0; i++) {
+    
+    vec3 col = palette(mod(length(uv) + i * 6.0 + uTime * 0.8, 1.0));
+    
+    float strength = sin(length(uv) * 8.0 + uTime)/8.0;
+    strength = abs(strength);
 
-    // final += strength;
-
-    gl_FragColor = vec4(final, 1.0);
+    finalColor += col * strength;
+}
+    gl_FragColor = vec4(finalColor, 1.0);
 }
